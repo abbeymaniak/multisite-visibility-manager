e03fdc16a906be6548e86615a393bd29c4c00422
personal <primastech101@gmail.com>
2025-08-25 20:19:28 +0100

    restructure header requirements

--- multisite-visibility-manager.php
+++ multisite-visibility-manager.php
@@ -1,37 +1,34 @@
 <?php
 
 /**
- *  Multisite Visibility Manager
+ * Multisite Visibility Manager
  *
- * Plugin Name:     Multisite Visibility Manager
- * Description:     This plugin is responsible for Managing search engine visibility across all subsites in the network from one settings page.
- * Plugin URI:		https://github.com/abbeymaniak/multisite-visibility-manager
- * Author:          Abiodun Paul Ogunnaike
- * Author URI:		https://linkedin.com/in/abiodun-paul-ogunnaike
- * Text Domain:     multisite-visibility-manager
- * Network: 		true
- * Domain Path:     /languages
- * Version:         1.0.0
- * prefix:          Multisite_Visibility_Manager
- * Donate:          https://www.buymeacoffee.com/abbeymaniak
- * License:         GPL v2 or later
- * License URI:     https://www.gnu.org/licenses/gpl-2.0.htmls
- * Requires PHP:    7.4
+ * @category Plugin
+ * @package  Multisite_Visibility_Manager
+ * @author   Abiodun Paul Ogunnaike <ayo_ogunnaike@yahoo.com>
+ * @license  http://www.gnu.org/licenses/gpl-2.0.txt GPLv2 or later
+ * @link     https://github.com/abbeymaniak/multisite-visibility-manager
+ *
+ * Plugin Name:       Multisite Visibility Manager
+ * Description:       Manage search engine visibility across all subsites in the network from one settings page.
+ * Plugin URI:        https://github.com/abbeymaniak/multisite-visibility-manager
+ * Author:            Abiodun Paul Ogunnaike
+ * Author URI:        https://linkedin.com/in/abiodun-paul-ogunnaike
+ * Text Domain:       multisite-visibility-manager
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * Network:           true
+ * Version:           1.0.0
+ * Requires PHP:      7.4
  * Requires at least: 6.0
- *
- *
- *
- * @package         Multisite_Visibility_Manager
- * @author  		Abiodun Paul Ogunnaike <ayo_ogunnaike@yahoo.com>
- *
+ * Donate:            https://www.buymeacoffee.com/abbeymaniak
  */
 
-// If the file is accessed directly abort script.
 defined('ABSPATH') || die('Unauthorized Access');
-
 
 // Include the main plugin class.
 require_once plugin_dir_path(__FILE__) . 'includes/class-multisite-visibility-manager.php';
+
 
 //instantiate the class
 $Multisite_Visibility_Manager = new Multisite_Visibility_Manager();
